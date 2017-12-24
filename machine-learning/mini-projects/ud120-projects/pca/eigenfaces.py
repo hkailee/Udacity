bac0d8c45a69f232a7a8c98aa97dd2a32e3df6eb
hkailee <leehongkai@gmail.com>
2017-12-24 23:28:15 +0800

    update

--- machine-learning/mini-projects/ud120-projects/pca/eigenfaces.py
+++ machine-learning/mini-projects/ud120-projects/pca/eigenfaces.py
@@ -66,12 +66,13 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random
 ###############################################################################
 # Compute a PCA (eigenfaces) on the face dataset (treated as unlabeled
 # dataset): unsupervised feature extraction / dimensionality reduction
-n_components = 150
+n_components = 50
 
 print "Extracting the top %d eigenfaces from %d faces" % (n_components, X_train.shape[0])
 t0 = time()
 pca = RandomizedPCA(n_components=n_components, whiten=True).fit(X_train)
 print "done in %0.3fs" % (time() - t0)
+print pca.explained_variance_ratio_
 
 eigenfaces = pca.components_.reshape((n_components, h, w))
 
