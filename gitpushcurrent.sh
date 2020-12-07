1803747657c1063796556c67bd88a6691e846e56
hkailee <leehongkai@gmail.com>
2020-12-07 21:35:34 +0800

    update new project

--- gitpushcurrent.sh
+++ gitpushcurrent.sh
@@ -3,5 +3,5 @@
 #git filter-branch --tree-filter 'rm -rf deep-learning/projects/other_mini_projects/cycleGAN/summer2winter_yosemite.zip' HEAD
 #git filter-branch --tree-filter 'rm -rf deep-learning/projects/other_mini_projects/DCGAN_SVHN/data/train_32x32.mat' HEAD
 git add .
-git commit -m "update mini-project"
+git commit -m "update new project"
 git push
