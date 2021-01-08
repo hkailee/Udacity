6d3bcdd2d493cc80dc0580ab210479e00e77bb88
hkailee <leehongkai@gmail.com>
2021-01-08 23:56:57 +0800

    update new project

--- natural_language_processing/projects/VUI_Speech_Recognizer/sample_models.py
+++ natural_language_processing/projects/VUI_Speech_Recognizer/sample_models.py
@@ -1,7 +1,7 @@
 from keras import backend as K
 from keras.models import Model
-from keras.layers import (BatchNormalization, Conv2D, Dense, Input, 
-    TimeDistributed, Activation, Bidirectional, SimpleRNN, GRU, LSTM, Dropout, Reshape, MaxPooling2D, Flatten)
+from keras.layers import (BatchNormalization, Conv1D, Dense, Input, 
+    TimeDistributed, Activation, Bidirectional, SimpleRNN, GRU, LSTM, Dropout, Reshape, MaxPooling1D, Flatten)
 
 def simple_rnn_model(input_dim, output_dim=29):
     """ Build a recurrent network for speech 
@@ -70,8 +70,10 @@ def cnn_rnn_model(input_dim, filters, kernel_size, conv_stride,
     print(model.summary())
     return model
 
+
+
 def cnn_output_length(input_length, filter_size, border_mode, stride,
-                       dilation=1):
+                       dilation=1, pooling=1):
     """ Compute the length of the output sequence after 1D convolution along
         time. Note that this function is in line with the function used in
         Convolution1D class from Keras.
@@ -81,6 +83,7 @@ def cnn_output_length(input_length, filter_size, border_mode, stride,
         border_mode (str): Only support `same` or `valid`.
         stride (int): Stride size used in 1D convolution.
         dilation (int)
+        pooling (int): Maxpooling size
     """
     if input_length is None:
         return None
@@ -90,7 +93,8 @@ def cnn_output_length(input_length, filter_size, border_mode, stride,
         output_length = input_length
     elif border_mode == 'valid':
         output_length = input_length - dilated_filter_size + 1
-    return (output_length + stride - 1) // stride
+    return (output_length + stride - 1) // (stride * pooling)
+
 
 def deep_rnn_model(input_dim, units, recur_layers, output_dim=29):
     """ Build a deep recurrent network for speech 
@@ -140,64 +144,92 @@ def bidirectional_rnn_model(input_dim, units, output_dim=29):
 
 def final_model(input_dim=161, 
                 filters=200,
-                kernel_size=1,
-                conv_stride=(1, 1),
-                conv_border_mode='valid',
+                kernel_size=3,
+                conv_stride=1,
+                conv_border_mode='same',
                 units=200, 
+                cnn_layers=3,
                 recur_layers=3, 
-                dropout_rate=0.3, 
+                layer_dropout_rate=0.3, 
+                recurrent_dropout_rate=0.3, 
+                maxpool_size=2,
                 output_dim=29):                    
     """ Build a deep network for speech 
     """
     # Main acoustic input
     input_data = Input(name='the_input', shape=(None, input_dim))
+    
     # TODO: Specify the layers in your network
     activation='relu'
     
-    # Add convolutional layer
+    """
+    # Add 2D convolutional layer (Failed - to come back later after graduation)
     input_shape = (13, 13, 1)
-    input_data = Reshape((13, 13, 1))(input_data)
+    
     conv_2d_1 = Conv2D(filters, kernel_size, 
                      strides=conv_stride, 
                      input_shape=input_shape,
                      padding=conv_border_mode,
                      activation=activation,
                      name='conv2d')(input_data)
-    pool_1 = MaxPooling2D(pool_size=(2, 2))(conv_2d_1)
+    pool_1 = MaxPooling2D(pool_size=(2, 2))(Reshape(input_shape)(onv_2d_1)
     
     # Add batch normalization and 
     bn_cnn = BatchNormalization(name='bn_pool_1')(pool_1)
-    flat = Flatten()(bn_cnn)
+    reshaped_bn_cnn = Reshape((36, 200))(bn_cnn)
+    """
     
-    for layer in range(recur_layers):
-        if layer == 0:
-            simple_rnn = Bidirectional(GRU(units, activation=activation,
-                                           return_sequences=True, implementation=2, 
-                                           name='rnn'+str(layer), 
-                                           dropout_U=dropout_rate), 
-                                       merge_mode='concat')(flat)
-            bn_rnn = BatchNormalization()(simple_rnn)
-            drop_out_rnn = Dropout(rate=dropout_rate)(flat)
+    # Add convolutional layer
+    temp_layer = input_data
+    
+    for i in range(cnn_layers):
+        # Add convolutional layer
+        conv_1d = Conv1D(filters, 
+                     kernel_size, 
+                     strides=conv_stride, 
+                     padding=conv_border_mode,
+                     activation=activation,
+                     name=f'conv1d_{i+1}')(temp_layer)
+
+        # Add batch normalization
+        bn_cnn = BatchNormalization(name=f'bn_conv_1d_{i+1}')(conv_1d)
+        
+        # Add dropout after convolution
+        if layer_dropout_rate > 0:
+            temp_layer = Dropout(layer_dropout_rate, name=f'cnn_dropout_{i+1}')(bn_cnn)
         else:
-            simple_rnn = Bidirectional(GRU(units, activation=activation,
+            temp_layer = bn_cnn
+    
+    
+    # Add max pooling 
+    pool_cnn = MaxPooling1D(maxpool_size, name=f'cnn_max_pool_{i+1}')(temp_layer)
+    
+    temp_layer = pool_cnn
+    for i in range(recur_layers):
+        simple_rnn = Bidirectional(GRU(units, activation=activation,
                                            return_sequences=True, implementation=2, 
-                                           name='rnn'+str(layer), 
-                                           dropout_U=dropout_rate), 
-                                       merge_mode='concat')(flat)
-            bn_rnn = BatchNormalization()(simple_rnn)
-            drop_out_rnn = Dropout(rate=dropout_rate)(flat)
+                                           name='rnn'+str(i), 
+                                           dropout_U=recurrent_dropout_rate), 
+                                           merge_mode='concat')(temp_layer)
+        bn_rnn = BatchNormalization()(simple_rnn)
+        # Add dropout after convolution
+        if layer_dropout_rate > 0:
+            temp_layer = Dropout(layer_dropout_rate, name=f'bn_rnn_dropout_{i+1}')(bn_rnn)
+        else:
+            temp_layer = bn_rnn
                     
     # Add a TimeDistributed(Dense(output_dim)) layer
-    time_dense = TimeDistributed(Dense(output_dim))(drop_out_rnn)        
+    time_dense = TimeDistributed(Dense(output_dim))(temp_layer)        
             
     # TODO: Add softmax activation layer
     y_pred = Activation('softmax',name='softmax')(time_dense)
 
     # Specify the model
     model = Model(inputs=input_data, outputs=y_pred)
+    
     # TODO: Specify model.output_length
     model.output_length = lambda x: cnn_output_length(
-                                  x, kernel_size, conv_border_mode, conv_stride)
+                                   x, kernel_size, conv_border_mode, conv_stride, pooling=maxpool_size)
 
     print(model.summary())
     return model
